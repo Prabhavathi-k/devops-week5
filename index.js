1d16ef5946c179b97b87a505ec658398e6889f52
Prabhavathi-k <147647486+Prabhavathi-k@users.noreply.github.com>
2024-12-10 21:41:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker from Jenkins!');
+ // res.send('Hello, Docker from Jenkins!');
+  res.send('Hello, this is update1 of file from Jenkins!');
 });
 
 app.listen(port, () => {
